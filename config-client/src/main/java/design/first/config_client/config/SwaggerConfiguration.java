6e3e8904e856cb7b4ca070ee247510945d422e97
First2019 <790867216@qq.com>
2020-12-31 10:28:07 +0800

    '联合测试'

---
+++ config-client/src/main/java/design/first/config_client/config/SwaggerConfiguration.java
@@ -0,0 +1,45 @@
+
+package design.first.config_client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+
+@Configuration
+@EnableSwagger2WebMvc
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean(value = "configClient--API")
+    @Order(value = 1)
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("design.first.config_client.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo groupApiInfo(){//主页显示
+        return new ApiInfoBuilder()
+                .title("我的接口文档~~~！！！")
+                .description("<div style='font-size:14px;color:red;'>swagger-bootstrap-ui-demo RESTful APIs</div>")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact("790867216@qq.com")
+                .version("1.0")
+                .build();
+    }
+
+
+}
